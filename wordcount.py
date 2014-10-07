29a07a20d54c58d4cac692b3fcd09a7b22b3e60a
Hackbright Student <info@hackbrightacademy.com>
2014-10-07 11:53:48 -0700

    Added wordcount.py short.txt twain.txt Initial commit.

---
+++ wordcount.py
@@ -0,0 +1,24 @@
+
+
+#def read_file(filename):
+    
+#    d = {}
+
+f = open('short.txt')
+for line in f:
+    word = line.strip().split()
+    #length = len(word)
+    #d[word] = length
+print word
+
+#def print_word(story):
+#    pass
+
+
+#def main():
+#    story = read_file('short.txt')
+#    print_word(story)
+
+
+#if __name__ == "__main__":
+#    main()
